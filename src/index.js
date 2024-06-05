844bc88f8f6c9039b6c5f83d2ad74bd67b2f29e2
minxng <minxng12@gmail.com>
2024-06-05 22:30:32 +0900

    form 연습

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import AppMentorsImmer from "./AppMentorsImmer";
+import AppWrap from "./AppWrap";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppMentorsImmer />
+    <AppWrap />
   </React.StrictMode>
 );
 
